4a015dff9ddf0cae027ee96558922a4ca654da50
Jhanavi <jeevithajaansrinivas@gmail.com>
2026-02-23 14:33:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+/ src/index.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Import posts router
+const postRouter = require('./routes/posts.routes');
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to Blogify API');
+});
+
+// Mount router
+app.use('/api/v1/posts', postRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ src/routes/post.routes.js
@@ -0,0 +1,12 @@
+// src/routes/posts.routes.js
+
+const express = require('express');
+const router = express.Router();
+
+// Import controller
+const postController = require('../controllers/posts.controller');
+
+// Route
+router.get('/', postController.getAllPosts);
+
+module.exports = router;
